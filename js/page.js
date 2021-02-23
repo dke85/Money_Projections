b6b6367a9eab9f3387cbef9d95091d5b0af52195
Dennis <11572625+dke85@users.noreply.github.com>
2021-02-22 19:58:09 -0500

    some style updates and first function that's not working

---
+++ js/page.js
@@ -0,0 +1,11 @@
+//Global Variables
+
+
+//Functions
+function createLayout(){
+    let combo = document.getElementById('netCalc').options.item[this.selectedOption].getAttribute('layout');
+
+    console.log(combo);
+};
+
+//Objects
