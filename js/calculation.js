b8a6c9d8a601fa9c53ad3da878a376daf2615e2e
DKE85 <delmore85@gmail.com>
2021-03-14 17:25:04 -0400

    Added second form and the js to make it functional as well as a default to remove previous forms based on selection or lack there of

--- js/calculation.js
+++ js/calculation.js
@@ -5,6 +5,18 @@ function calcDeducted(){
     return c;
 }
 
+function calcNetPay(){
+    let a = document.getElementById("netPayGross").value;
+    let b = document.getElementById("netPayDeducted").value;
+    let c = a-b;
+    return c;
+}
+
 function deductedCalcJS(){
     document.getElementById("deductedCalc").value = calcDeducted();
+}
+
+
+function netPayCalcJS(){
+    document.getElementById("netPayCalc").value = calcNetPay();
 }
--- js/page.js
+++ js/page.js
@@ -5,11 +5,19 @@
 function createLayout(){
     let combo = document.getElementById('netCalc').options[netCalc.selectedIndex].getAttribute('layout');
     if (combo == 1){
+        netPay.style.display = "none";
         deducted.style.display = "flex";
 
     }
+    else if (combo == 2){
+        deducted.style.display = "none";
+        netPay.style.display = "flex";
 
-    console.log(combo);
+    }
+    else{
+        deducted.style.display = "none";
+        netPay.style.display = "none";
+    }
 }
 
 //Objects
